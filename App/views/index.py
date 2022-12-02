f3b3dc153f2e55c76d769170e5e68f9fa68fceaf
TheGreatDevourer <anthonygould2304@outlook.com>
2022-12-02 00:28:18 -0400

    updated routes to point to index

--- App/views/index.py
+++ App/views/index.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, redirect, render_template, request, send_from_directory
+from flask import Blueprint, redirect, render_template, request, send_from_directory, url_for
 from flask_login import login_required, current_user
 
 from App.controllers import (
     get_staff,
+    get_student,
     get_staff_notifications,
     get_accepted_request_by_staffID
 )
@@ -30,4 +31,6 @@ def homepage():
         notifs = get_staff_notifications(userID)
         accepted_req_students=get_accepted_request_by_staffID(userID)
         return render_template('notif/index.html', notifs=notifs, accepted_req_students=accepted_req_students)
+    if get_student(userID):
+        return redirect(url_for('requestRec_views.request_page'))
     return render_template('index.html')
